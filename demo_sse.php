bae664790099bf3a22c34d98a7b57e29fb368fea
Vu Pham <vuph79@gmail.com>
2019-06-28 16:44:22 +0700

    add new works

--- demo_sse.php
+++ demo_sse.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
+// header('Access-Control-Allow-Origin: *');
 
 $time = date('r');
 echo "data: The server time is: {$time}\n\n";
